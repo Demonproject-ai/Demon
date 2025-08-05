ef3bd00f131b478ec4efcaf9b02de5cf6dd79c80
Demonproject-ai <camil@ostoya.se>
2025-08-05 13:37:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Demon from './Demon';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Demon />
+  </React.StrictMode>
+);
